ad3fc2195704198f055e7c1af212259ec50fe679
hoainam-dev <nam54529@donga.edu.vn>
2024-10-02 16:02:06 +0700

    fix: lazy loading

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import Provider from './store/Provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.Suspense fallback={<div>Loading...</div>}>
     <Provider>
         <App />
     </Provider>
-  </React.StrictMode>
+  </React.Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
